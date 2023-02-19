d9ec4c4a240f7426a8e5d0986cdf7ba81d55117e
Syarif Hidayatulloh <id.syarif@gmail.com>
2023-02-19 13:51:21 +0700

    refactor code math basic

--- src/MathBasic.js
+++ src/MathBasic.js
@@ -1,5 +1,5 @@
 const MathBasic = {
-    add: (...args) => {
+    _validation(args)  {
         if (args.length !== 2 ) {
             throw new Error('fungsi add hanya menerima dua parameter');
         }
@@ -10,48 +10,26 @@ const MathBasic = {
             throw new Error('fungsi hanya menerima parameter number')
         }
 
-        return a + b;
-
+        return args
     },
 
-    subtract: (...args) => {
-        if (args.length !== 2 ) {
-            throw new Error('fungsi subrtact hanya menerima dua parameter');
-        }
+    add(...args) {
+        const [a, b] = this._validation(args)
+        return a + b;
+    },
 
-        const [a, b] = args
-
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('fungsi hanya menerima parameter number');
-        }
-
+    subtract(...args) {
+        const [a, b] = this._validation(args)
         return a - b;
     },
 
-    multiply: (...args) => {
-        if (args.length !== 2) {
-            throw new Error('fungsi multiply hanya menerima 2 parameters');
-        }
-
-        const [a, b] = args
-
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('fungsi multiply hanya menerima parameter number');
-        }
-
+    multiply(...args) {
+        const [a, b] = this._validation(args)
         return a * b;
     },
 
-    divide: (...args) => {
-        if (args.length !== 2) {
-            throw new Error('fungsi divide menerima 2 parameters');
-        }
-
-        const [a, b] = args;
-
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('fungsi divide hanya menerima parameter number');
-        }
+    divide(...args) {
+        const [a, b] = this._validation(args)
 
         if (a === 0 || b === 0) {
             throw new Error('fungsi tidak menerima parameter zero')
